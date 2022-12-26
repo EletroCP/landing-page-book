47e912f29980726105200eb580992c9144b6c67d
EletroCP <brernolopes@hotmail.com>
2022-12-26 10:14:55 -0300

    :sparkles: feat: create a base structure site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from './logo.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h2>Nome Livro</h2>
       </header>
+      <main>
+        <img src={ Logo } alt="Author" style={{ width: '500px'}} />
+        <h4>Start of synopsis ...</h4>
+        <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+          aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+          non proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum."
+        </p>
+      </main>
+      <section>
+        <h3>
+          Author Name
+        </h3>
+        <p>
+          Aboth the author: Lorem ipsum dolor sit amet, consectetur adipiscing elit ...
+        </p>
+      </section>
+      <section>
+        <h3>
+          Buy the book
+        </h3>
+        <p>
+          To continue reading the book, simply purchase its digital version right here.
+        </p>
+        <a href="https://www.packzin.com.br/" target="_blank" rel='noreferrer'>Pursh</a>
+      </section>
     </div>
   );
 }
